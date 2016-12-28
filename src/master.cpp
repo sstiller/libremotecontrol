e3c74caf229c615ef823a01d39119c72b6471f89
Sandro Stiller <sandro.stiller@gmx.de>
2016-12-28 19:15:15 +0100

    added UDP control server

---
+++ src/master.cpp
@@ -0,0 +1,41 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-  */
+/*
+ * master.cpp
+ * Copyright (C) 2016 Sandro Stiller <sandro.stiller@gmx.de>
+ *
+ * libremotectl is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libremotectl is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "master.h"
+
+Master::Master(std::shared_ptr<boost::asio::io_service> ioService, const std::string& ipAddress, unsigned short port)
+: ioService(ioService),
+  udpSocket(*ioService)
+{
+  boost::asio::ip::udp::resolver resolver(*ioService);
+  boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), ipAddress, std::to_string(port));
+  receiverEndpoint = *resolver.resolve(query);  
+    
+  udpSocket.open(boost::asio::ip::udp::v4());
+
+}
+
+void Master::sendCommand(const ControlCommand& command)
+{
+  udpSendData = command.serialize();
+  udpSocket.send_to(boost::asio::buffer(udpSendData), receiverEndpoint);
+}
+
+
+
---
+++ src/master.h
@@ -0,0 +1,50 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-  */
+/*
+ * master.h
+ * Copyright (C) 2016 Sandro Stiller <sandro.stiller@gmx.de>
+ *
+ * libremotectl is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libremotectl is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _MASTER_H_
+#define _MASTER_H_
+
+// stl
+#include <memory>
+
+// boost
+#include <boost/asio.hpp>
+
+// local includes
+#include "control-command.h"
+
+
+class Master
+{
+public:
+  Master(std::shared_ptr<boost::asio::io_service> ioService, const std::string& ipAddress, unsigned short port = 2345);
+
+  void sendCommand(const ControlCommand& command);
+   
+private:
+  std::shared_ptr<boost::asio::io_service> ioService;
+  boost::asio::ip::udp::socket udpSocket;
+  boost::asio::ip::udp::endpoint receiverEndpoint;
+     
+  std::vector<char> udpSendData;
+   
+};
+
+#endif // _MASTER_H_
+
